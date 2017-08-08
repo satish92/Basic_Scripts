2fda2993f324b15d0551728c83a1b976d1235dc1
satish92 <satish.chow9@gmail.com>
2017-08-08 15:19:43 -0500

    updated
    SCP transfer_validation_comparision

--- SCP_source_dest.sh
+++ SCP_source_dest.sh
@@ -1,15 +1,25 @@
 #importing from multiple remote hosts to single destination 
+#directory indicates to replace with your desired directory 
 
 for i in " your host names"
 do
 #creating temporary directory in local machine
-mkdir -p /tmp/scripts/$i
+mkdir -p /directory/$i
 echo " directory created for $i"
 echo "file transfer started for $i"
-scp skumar21@$i:/opt/spacecmd/sync-temp/* /tmp/scripts/$i
+scp username@$i:/directory/* /tmp/scripts/$i
 echo "file transfer completed for $i"
 
+#To validate transferred files and add an extra string(host naem) for each line and save as a file
+# $i from below line respresents from above for loop
+ssh username@$i ls -l /directory | head -1 | sed "s/^/$i /"  >> b.txt
+
+#to validate tansferred files from remote hosts to local machine and appending host names on each line and saving it as a seperate file
+ls -l "/tmp/scripts/$i" | head -1| sed "s/^/$i /" >> c.txt
+
 done
+#to identify difference between two generated files
+diff file1 file2
 
 exit 0
 ~
